55a47418bbfec23c81cc61dae281f056c87a7d47
VAISHNAVI M <vaishnavim1194@gmail.com>
2024-05-25 08:41:22 +0530

    Routing_update

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ export default function App() {
           <Route path='/' element={<LandingPage />} />
           <Route path='/landingpage' element={<LandingPage />} />
           <Route path='/alltasks' element={<TaskSB />} />
+          <Route path='/pendingtasks' element={<TaskSB filterType="progress" />} />
+          <Route path='/completedtasks' element={<TaskSB filterType="completed" />} />
+          <Route path='/yettasks' element={<TaskSB filterType="yet" />} />
           <Route path='/users' element={<UserSB />} />
         </Routes>
       </div>
